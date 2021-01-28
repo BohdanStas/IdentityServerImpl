4f01c106147e3ac5f17f8f3208a507305b1c85b8
UpDown12 <45355822+UpDown12@users.noreply.github.com>
2021-01-28 12:07:32 +0200

    Added identity server login functionality

--- IdentityServerAuth/IdentityServer/Controllers/HomeController.cs
+++ IdentityServerAuth/IdentityServer/Controllers/HomeController.cs
@@ -5,21 +5,54 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace IdentityServer.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-            _logger = logger;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(model.UserName);
+
+                if (user==null)
+                {
+                    ModelState.AddModelError("","User not found");
+                    return View(model);
+                }
+
+                var result =await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+
+                ModelState.AddModelError("","Something went wrong");
+                return View(model);
+            }
+
+            return View(model);
         }
 
     }
--- IdentityServerAuth/IdentityServer/Models/LoginViewModel.cs
+++ IdentityServerAuth/IdentityServer/Models/LoginViewModel.cs
@@ -7,5 +7,10 @@ namespace IdentityServer.Models
 {
     public class LoginViewModel
     {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
     }
 }
